126be877f269ae7d4595c1ab0125735f939449fa
tyler <tylermontobbio@gmail.com>
2023-04-08 16:49:45 -0700

    nav styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,25 @@
 import rocket from "../img/rocket.png"
+import { useState } from 'react';
 
 export default function Nav() {
+
+    const [navVisible, setNavVisible] = useState(false)
+    const toggleNavVisible = () => {
+        setNavVisible(!navVisible)
+    }
+
     return (
         <div id="nav">
             <h1>Tyler Montobbio</h1>
-            <img src={rocket} alt="rocket" />
+            <span>
+                {navVisible && <span>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </span>}
+                <img src={rocket} alt="rocket" onClick={toggleNavVisible} />
+            </span>
         </div>
     )
 }
