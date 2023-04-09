88aadc4574cbf035dbbb0f085976752a2539c9d1
tyler <tylermontobbio@gmail.com>
2023-04-08 18:06:32 -0700

    fixed nav buttons and spacing

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,8 @@ function App() {
         <Nav />
         <Home />
         <About />
-        <Contact />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import rocket from "../img/rocket.png"
+import { Link } from "react-scroll"
 import { useState } from 'react';
 
 export default function Nav() {
@@ -11,15 +12,24 @@ export default function Nav() {
     return (
         <div id="nav">
             <h1>Tyler Montobbio</h1>
-            <span>
-                {navVisible && <span>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </span>}
+            <div className="spacer" />
+            <div id="menu">
                 <img src={rocket} alt="rocket" onClick={toggleNavVisible} />
-            </span>
+                {navVisible && <span>
+                    <Link to="home" spy={true} smooth={true} duration={900} onClick={toggleNavVisible}>
+                        <li>Home</li>
+                    </Link>
+                    <Link to="about" spy={true} smooth={true} duration={900} onClick={toggleNavVisible}>
+                        <li>About</li>
+                    </Link>
+                    <Link to="projects" spy={true} smooth={true} duration={900} onClick={toggleNavVisible}>
+                        <li>Projects</li>
+                    </Link>
+                    <Link to="contact" spy={true} smooth={true} duration={900} onClick={toggleNavVisible}>
+                        <li>Contact</li>
+                    </Link>
+                </span>}
+            </div>
         </div>
     )
 }
